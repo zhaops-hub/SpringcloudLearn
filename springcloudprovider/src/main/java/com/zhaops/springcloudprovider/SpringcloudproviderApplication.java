c515108020593bbf01e3613acfdf4373f2baeaba
zhaopeisheng <zhaops@soyuan.com.cn>
2020-10-15 18:39:07 +0800

    添加项目

---
+++ springcloudprovider/src/main/java/com/zhaops/springcloudprovider/SpringcloudproviderApplication.java
@@ -0,0 +1,13 @@
+package com.zhaops.springcloudprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudproviderApplication.class, args);
+	}
+
+}
